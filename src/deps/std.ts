ce400e7b4d1f780708565a1be817ee03996a637b
Jacky Nguyen <nktpro@gmail.com>
2023-11-02 17:14:03 -0400

    Update dependencies

--- src/deps/std.ts
+++ src/deps/std.ts
@@ -1,5 +1,5 @@
-export { assertEquals, assertExists, assertRejects, assertThrows } from "https://deno.land/std@0.202.0/assert/mod.ts";
-export { groupBy } from "https://deno.land/std@0.202.0/collections/group_by.ts";
-export { mapEntries } from "https://deno.land/std@0.202.0/collections/map_entries.ts";
-export { deferred } from "https://deno.land/std@0.202.0/async/deferred.ts";
-export { join as pathJoin } from "https://deno.land/std@0.202.0/path/join.ts";
+export { assertEquals, assertExists, assertRejects, assertThrows } from "https://deno.land/std@0.205.0/assert/mod.ts";
+export { groupBy } from "https://deno.land/std@0.205.0/collections/group_by.ts";
+export { mapEntries } from "https://deno.land/std@0.205.0/collections/map_entries.ts";
+export { deferred } from "https://deno.land/std@0.205.0/async/deferred.ts";
+export { join as pathJoin } from "https://deno.land/std@0.205.0/path/join.ts";
--- src/deps/utils.ts
+++ src/deps/utils.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/x/utils@2.15.3/exec_utils.ts";
-export * from "https://deno.land/x/utils@2.15.3/cli_utils.ts";
-export * from "https://deno.land/x/utils@2.15.3/deps/typebox.ts";
+export * from "https://deno.land/x/utils@2.18.1/exec_utils.ts";
+export * from "https://deno.land/x/utils@2.18.1/cli_utils.ts";
+export * from "https://deno.land/x/utils@2.18.1/deps/typebox.ts";
